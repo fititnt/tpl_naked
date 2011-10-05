4c144a9f701a38ad954f9b43caf319a910bd2759
fititnt <emerson@webdesign.eng.br>
2011-10-05 17:10:47 -0200

    Fixed template positions

--- index.php
+++ index.php
@@ -6,12 +6,16 @@
  * @license         GNU General Public License version 3. See license.txt
  */
 defined('_JEXEC') or die('Restricted access');
-?>
-<?php if($this->countModules('atomic-topquote')) : ?>
+
+if($this->countModules('before')) : ?>
 <jdoc:include type="modules" name="before" style="none" />
 <?php endif; ?>
 <jdoc:include type="message" />
 <jdoc:include type="component" />
-<?php if($this->countModules('atomic-topquote')) : ?>
+<?php if($this->countModules('after')) : ?>
 <jdoc:include type="modules" name="after" style="none" />
 <?php endif; ?>
+<?php if($this->countModules('debug')) : ?>
+<jdoc:include type="modules" name="debug" style="none" />
+<?php endif; ?>
+
